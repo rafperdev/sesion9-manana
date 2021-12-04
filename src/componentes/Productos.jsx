6d3328b7aeb4cd168f302cb6182c191be769cffe
Rafael Perez <rbperez@uninorte.edu.co>
2021-12-04 09:38:46 -0500

    Peticiones APIs Guardar, Consultar

--- src/componentes/Productos.jsx
+++ src/componentes/Productos.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { consumir } from '../api/comentarios';
 
 export function Productos() {
     let listado;
@@ -7,7 +8,7 @@ export function Productos() {
     const preRef = useRef();
     const stockRef = useRef();
     const [success, setSuccess] = useState(false);
-    const guardar = () => {
+    const guardarCache = () => {
         const nom = nomRef.current.value;
         const pre = preRef.current.value;
         const stock = stockRef.current.value;
@@ -22,6 +23,42 @@ export function Productos() {
         setSuccess(true);
         setTimeout(() => setSuccess(false), 2000)
     }
+    // useEffect(() => {
+    //     const peticion = async () => {
+    //         const data = await consumir();
+    //         nomRef = data.nom;
+    //         // setListComments(data);
+    //     }
+    //     peticion();
+    // },[])
+    function consultar() {
+        const nom = nomRef.current.value;
+        fetch(`http://localhost:8081/producto/consultar/${nom}`)
+            .then(data => data.json())
+            .then(data => {
+                if (data.estado === "ok") {
+                    preRef.current.value = data.prod.price;
+                    stockRef.current.value = data.prod.stock;
+                } else {
+                    alert(data.msg);
+                }
+            })
+    }
+
+    function guardar() {
+        const nom = nomRef.current.value;
+        const pre = preRef.current.value;
+        const stock = stockRef.current.value;
+        fetch(`http://localhost:8081/producto/guardar`, {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({ nombre: nom, precio: pre, stock })
+        }).then(data => data.json())
+            .then(data => {
+                setSuccess(true);
+                setTimeout(() => setSuccess(false), 2000);
+            })
+    }
     return (
         <form>
             {success && <div class="alert alert-success" role="alert">Guardado con éxito!
@@ -33,6 +70,7 @@ export function Productos() {
             <label htmlFor="">Stock</label>
             <input ref={stockRef} className="form-control" type="text" />
             <button className="btn btn-primary" type="button" onClick={guardar}>Guardar</button>
+            <button className="btn btn-primary" type="button" onClick={consultar}>Consultar</button>
             <Link className="btn btn-primary" to="/producto/lista" >Listar</Link>
             <Link to="/comments" >Comentarios</Link>
         </form>
